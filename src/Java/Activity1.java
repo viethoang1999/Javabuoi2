531ba7a624df75eea735fd03d314cea47d15ca3a
Viet Hoang <viethoang.vt99@gmail.com>
2020-04-18 13:37:25 +0700

    day

--- src/Java/Activity1.java
+++ src/Java/Activity1.java
@@ -1,5 +1,11 @@
 package Java;
 
+import java.util.Scanner;
+
 public class Activity1 {
+    public static void main(String[] arg){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Mời bạn nhập n");
+    }
 
 }
