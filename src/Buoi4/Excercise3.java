66d5282689c909b864bb0ec1a3daffa3c9ca2de0
Viet Hoang <viethoang.vt99@gmail.com>
2020-04-23 22:41:31 +0700

    đẩy Excerise 3

--- src/Buoi4/Excercise3.java
+++ src/Buoi4/Excercise3.java
@@ -3,13 +3,18 @@ package Buoi4;
 import java.util.Scanner;
 
 public class Excercise3 {
-    public static void main(String[] arg){
-        Scanner scanner=new Scanner(System.in);
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println(" Mời bạn nhập n: ");
-        int n=scanner.nextInt();
-        for(int i=1;i<=n;i++){
-            for (int j=1;j<=n-1;j++){
-                System.out.print("*");
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || j == 1 || i == n || j == n || i == j || n - i == j) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
             }
             System.out.println("");
         }
